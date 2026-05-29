45478d2f0fa541cd7f01b72b7e0cd4e3466754a3
jaye2720 <jaye2720@users.noreply.github.com>
2026-05-29 18:41:23 +0800

    Site updated: 2026-05-29 18:41:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/01/如何在Linux运行Windows exe软件/","2025/09/30/ARP断网攻击与监听/"];function toRandomPost(){
+var posts=["2025/09/30/ARP断网攻击与监听/","2025/03/01/如何在Linux运行Windows exe软件/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
